93af3068be7614b89ff8590409cd714cf6f1e047
wiliancb <wilian.barbosa@matera.com>
2011-09-14 22:28:19 -0300

    Javadoc added for TestCase class.

--- core/src/main/java/com/github/srec/command/TestCase.java
+++ core/src/main/java/com/github/srec/command/TestCase.java
@@ -10,10 +10,13 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for
  * the specific language governing permissions and limitations under the License.
  */
-
 package com.github.srec.command;
 
 /**
+ * A TestCase defines a set of inputs/outputs, preconditions, 
+ * postconditions and commands that are executed for a test objective.
+ * They are declared in XML files.
+ * 
  * @author Victor Tatai
  */
 public class TestCase {
